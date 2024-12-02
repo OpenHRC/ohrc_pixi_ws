333512b39e71276bfa447f3ef457c134a3b5c98d
Shunki Itadera <s.itadera@aist.go.jp>
2024-12-02 10:16:50 +0900

    new file:   docker/Dockerfile

---
+++ docker/ros_entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# setup ros environment
+source "/ros2_ws/install/setup.bash" --
+
+function get_started () {
+    marker_teleoperation_ur5e
+}
+
+function marker_teleoperation_ur5e () {
+    ur5e_sim & marker_teleoperation
+}
+
+function ur5e_sim(){
+    ros2 launch ur_simulation_gz ur_sim_control.launch.py initial_joint_controller:=forward_velocity_controller launch_rviz:=false
+}
+
+function marker_teleoperation(){
+    ros2 launch ohrc_teleoperation marker_teleoperation.launch.py
+}
+
+
+$@
