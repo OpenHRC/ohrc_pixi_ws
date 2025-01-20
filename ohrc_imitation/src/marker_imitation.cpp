0d068295aedea5652e2bdd73bc4fbe3e19b088a9
Shunki Itadera <s.itadera@aist.go.jp>
2025-01-20 19:20:20 +0900

    clean

--- ohrc_imitation/src/marker_imitation.cpp
+++ ohrc_imitation/src/marker_imitation.cpp
@@ -1,10 +1,5 @@
-
-#include "ohrc_control/single_interface.hpp"
-#include "ohrc_imitation/ohrc_imitation.hpp"
+#include "ohrc_imitation/single_imitation_controller.hpp"
 #include "ohrc_teleoperation/marker_interface.hpp"
-
-template <class T>
-class SingleImitationController : virtual public SingleInterface<T>, virtual public ImitationController {};
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
